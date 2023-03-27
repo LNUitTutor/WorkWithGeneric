62d1e32610397ff55e4c565d24afd82fab8c4a75
LNUitTutor <lnu.it.tutor@gmail.com>
2023-03-27 09:40:32 +0300

    First generic alg

---
+++ WorkWithGeneric/Algorithms.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithGeneric
+{
+    public static class Algorithms
+    {
+        public static double TotalVolume<T>(T[] objects)
+        {
+            double Sum = 0.0;
+            for (int i = 0; i < objects.Length; ++i) Sum += (objects[i] as Shape3D).Volume();
+            return Sum;
+        }
+    }
+}
--- WorkWithGeneric/Program.cs
+++ WorkWithGeneric/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WorkWithGeneric
 {
@@ -7,11 +8,22 @@ namespace WorkWithGeneric
         static void Main(string[] args)
         {
             Shape3D[] arr = { new Cylinder(), new Parallelepiped(), 
-                new Parallelepiped(1, 2, 3), new Cylinder(3, 5) };
+                new Parallelepiped(1, 2, 3), new Cylinder(3, 5), new Parallelepiped(4,4,1) };
             for (int i = 0; i < arr.Length; ++i) Console.WriteLine(arr[i]);
             double sum = 0.0;
             for (int i = 0; i < arr.Length; ++i) sum += arr[i].Volume();
             Console.WriteLine($" sum = {sum}");
+            Console.WriteLine($" Sum = {Algorithms.TotalVolume(arr)}");
+            List<Parallelepiped> P = new List<Parallelepiped>();
+            for (int i = 0; i < arr.Length; ++i)
+                if (arr[i] is Parallelepiped) P.Add(arr[i] as Parallelepiped);
+            Console.WriteLine($" Pum = {Algorithms.TotalVolume(P.ToArray())}");
+
+            List<Cylinder> C = new List<Cylinder>();
+            for (int i = 0; i < arr.Length; ++i)
+                if (arr[i] is Cylinder) C.Add(arr[i] as Cylinder);
+            Console.WriteLine($" Cum = {Algorithms.TotalVolume(C.ToArray())}");
+
         }
     }
 }
