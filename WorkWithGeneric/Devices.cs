416b8dca76b8fe2802364e70b6fb613f3608982c
LNUitTutor <lnu.it.tutor@gmail.com>
2023-03-27 17:41:21 +0300

    Flash added

---
+++ WorkWithGeneric/Devices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkWithGeneric
+{
+    public class FlashMemory : IVolumable
+    {
+        string producer;
+        int capacity;
+        public FlashMemory(string prod = "Kingston", int cap = 16)
+        {
+            producer = prod; capacity = cap;
+        }
+        public string Producer
+        {
+            get => producer;
+        }
+        public int Capacity
+        {
+            get => capacity;
+        }
+        public override string ToString()
+        {
+            return $"Flas({producer},{capacity}Gb)";
+        }
+        public double Volume()
+        {
+            return this.Capacity;
+        }
+    }
+}
